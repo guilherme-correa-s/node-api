baa06d5b521927456b6804ccecfcfcff16a5f9cd
Guilherme C <guilhermescorrea@hotmail.com.br>
2021-08-16 23:03:29 -0300

    find

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,15 +3,23 @@
 const moongose = require('mongoose');
 const Product = moongose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product.find({active:true}, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+};
+
 exports.post = (req, res, next) => {
     let product = new Product(req.body);
     product.save()
-    .then(x => {
-        res.status(201).send({message:"Sucesso ao cadastrar produto.", data: x});
-    }).catch(e => {
-        res.status(201).send({message: 'Falha ao cadastrar produto.', data: e});
-    });
-
+        .then(x => {
+            res.status(201).send({message:"Sucesso ao cadastrar produto.", data: x});
+        }).catch(e => {
+            res.status(201).send({message: 'Falha ao cadastrar produto.', data: e});
+        });
 };
 
 exports.put = (req, res, next) => {
