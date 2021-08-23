414980bee60c54404e63a39e76d619fa89280d8c
Guilherme Correa <guilhermescorrea@hotmail.com.br>
2021-08-22 21:45:05 -0300

    fluent validator

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const uri = 'mongodb+srv://guiadm:guiteste@node-store.utlje.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const uri =
+  "mongodb+srv://guiadm:guiteste@node-store.utlje.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const Product = require('./models/product');
+const Product = require("./models/product");
 
-const indexRoute = require('./routes/index-route');
-const productRoute = require('./routes/product-route');
+const indexRoute = require("./routes/index-route");
+const productRoute = require("./routes/product-route");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
-app.use('/', indexRoute);
-app.use('/products', productRoute);
+app.use("/", indexRoute);
+app.use("/products", productRoute);
 
 module.exports = app;
