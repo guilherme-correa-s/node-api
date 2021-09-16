26f7d1206415dd108cbec3337c6b7d3ab759f841
Guilherme C <guilhermescorrea@hotmail.com.br>
2021-09-15 22:32:23 -0300

    heroku

--- bin/server.js
+++ bin/server.js
@@ -2,8 +2,6 @@
 
 const app = require('../src/app');
 const http = require('http');
-const debug = require('debug')('nodestr:server');
-
 
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
@@ -13,7 +11,6 @@ const server = http.createServer(app);
 server.listen(port ,() => {
     console.log(`Rodando na porta${port}`);
 });
-server.on('listening', onListening)
 
 function normalizePort(val) {
     const port = parseInt(val, 10);
@@ -27,12 +24,4 @@ function normalizePort(val) {
     }
 
     return false;
-};
-
-function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug('listening on ' + bind);
 };
