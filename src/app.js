2a1fdb0b8e0971f4e1bc78c58512e73db39adb55
Guilherme C <guilhermescorrea@hotmail.com.br>
2021-09-15 22:25:27 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -28,4 +28,20 @@ app.use("/products", productRoute);
 app.use("/customers", customerRoute);
 app.use("/orders", orderRoute);
 
+app.all("*", function (req, res) {
+    res.status(404).send({ message: "Endpoint was not found" });
+});
+app.all("/*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header("Access-Control-Allow-Methods", "*");
+
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, x-access-token"
+    );
+
+    next();
+});
+
 module.exports = app;
